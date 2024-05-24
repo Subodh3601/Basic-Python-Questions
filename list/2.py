41c415ecbb72b7917d41679d44499de090867955
subodh singh <subsin360@gmail.com>
2024-05-24 16:23:42 +0530

    question added

--- list/2.py
+++ list/2.py
@@ -1,5 +1,19 @@
 # liner search 
 """
-fins the given input in list if finds then return index if not then return -1
+find the given input in list if finds then return index if not then return -1
 
 """
+n = input("enter the space separated list:").split()
+
+m = int(input('enter the number to search:'))
+index = []
+for i in range(len(n)):
+    if m == int(n[i]):
+        index.append(i)
+        continue
+if len(index)>0:
+    print(index)
+else:
+    print(-1)        
+    
+        
